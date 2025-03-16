0b497b8803ade6487947e2089889568108279dc4
Burness <bsiii102798@gmail.com>
2025-03-16 15:29:10 -0700

    Header-component

--- localbooks/src/components/Header.tsx
+++ localbooks/src/components/Header.tsx
@@ -2,8 +2,14 @@ import Link from "next/link";
 
 export default function Header() {
     return(
-        <header>
-            <Link href="">LocalBook</Link>
+        <header className="border-b p-4 flex items-center justify-between">
+            <Link className="text-blue-600 font-bold text-2xl" href="/">LocalBook</Link>
+            <nav className="*:text-blue-600 flex gap-4 *:border *:rounded  *:px-2 *:py-1">
+                <button className="">Post book</button>
+                <button className="">Login</button>
+                <button className="">Register</button>
+                <button className="">Cart</button>
+            </nav>
         </header>
     )
 }
