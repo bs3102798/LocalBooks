78f3763bcdc1c1e15d06813fa287551b3536ce84
Burness <bsiii102798@gmail.com>
2025-03-16 16:12:59 -0700

    HeaderComponentIcons

--- localbooks/src/components/Header.tsx
+++ localbooks/src/components/Header.tsx
@@ -1,14 +1,26 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faBook } from "@fortawesome/free-solid-svg-icons/faBook";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
 export default function Header() {
-    return(
+    return (
         <header className="border-b p-4 flex items-center justify-between">
-            <Link className="text-blue-600 font-bold text-2xl" href="/">LocalBook</Link>
-            <nav className="*:text-blue-600 flex gap-4 *:border *:rounded  *:px-2 *:py-1">
-                <button className="">Post book</button>
-                <button className="">Login</button>
-                <button className="">Register</button>
-                <button className="">Cart</button>
+            <Link className="text-[#3F2E56] font-bold text-2xl inline-flex items-center gap-1 px-2" href="/">
+            <FontAwesomeIcon icon={faBook} className="h-6" />
+            <span>LocalBook</span>
+            </Link>
+            <nav className="flex gap-4  *:rounded  *:py-1">
+                <button className="border border-[#3F2E56] text-[#3F2E56] inline-flex items-center gap-1 px-2">
+                    <FontAwesomeIcon  icon={faPlus} className="h-4" />
+                    <span>
+                        Post book
+                    </span>
+                </button>
+                <span className="text-[#3F2E56] text-2xl">|</span>
+                <button className="border-0 ">Sign up</button>
+                <button className="bg-[#3F2E56] text-white border-0 px-6">Login</button>
+                <button className="text-[#3F2E56]">Cart</button>
             </nav>
         </header>
     )
