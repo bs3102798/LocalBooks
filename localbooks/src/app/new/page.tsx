0d9d6b149d5b6fbdfb9d63ef192ec797aa05f8fb
Burness <bsiii102798@gmail.com>
2025-03-18 23:51:01 -0700

    NewBookPage

--- localbooks/src/app/new/page.tsx
+++ localbooks/src/app/new/page.tsx
@@ -1,19 +1,28 @@
 export default function NewBookPage() {
     return (
-        <form action=''>
-            <h1>Add Your Book</h1>
-            <input type='text' placeholder="Title" />
-            <input type='number' placeholder="Price" />
-            <select>
-                <option selected disabled value="">Select a genre</option>
+        <form action='' className="max-w-96 mx-auto flex">
+            <div>photos uploading</div>
+            
+            <div>
+            <label htmlFor="titleIn">Title</label>
+            <input id="titleIn" type='text' placeholder="Title" />
+            <label htmlFor="priceIn">Price</label>
+            <input id="priceIn" type='number' placeholder="Price" />
+            <label htmlFor="categoryIn">Category</label>
+            <select defaultValue="">
+                <option disabled value="">Select a genre</option>
                 {/* place font awsome images later */}
                 <option value="">action</option>
                 <option value="">historic</option>
                 <option value="">romance</option>
                 <option value="">comedy</option>
             </select>
+
+            <label htmlFor="descriptionIn">description</label>
             <textarea name="" id="" placeholder="Descripiton"></textarea>
-            <textarea name="" id="" placeholder="Mobile + 34 363 869 2945"></textarea>
+            <label htmlFor="contactIn">Phone</label>
+            <textarea id="contactIn" name="" placeholder="Mobile + 34 363 869 2945"></textarea>
+            </div>
         </form>
     )
 
