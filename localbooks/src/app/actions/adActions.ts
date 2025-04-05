ef1c1ccbe4a91e75aadf8e2d93be53f28697e52c
Burness <bsiii102798@gmail.com>
2025-04-04 21:14:29 -0700

    AdModel.create

--- localbooks/src/app/actions/adActions.ts
+++ localbooks/src/app/actions/adActions.ts
@@ -1,8 +1,22 @@
 "use server"
 
-export async function createAd(formData: FormData) {
-    const {files, location, ...data} = Object.fromEntries(formData)
+import { AdModel } from "@/models/Ad";
+import mongoose from "mongoose";
 
-    console.log({ files, location, data})
-    return(true);
+async function connect() {
+    return mongoose.connect(process.env.MONGODB_URL as string)
+}
+
+export async function createAd(formData: FormData) {
+    const { files, location, ...data } = Object.fromEntries(formData)
+    await connect();
+    await AdModel.create({
+        files: JSON.parse(files as string),
+        location: JSON.parse(location as string),
+        ...data
+
+    })
+
+    //console.log({ files, location, data})
+    return (true);
 }
--- localbooks/src/models/Ad.ts
+++ localbooks/src/models/Ad.ts
@@ -1,13 +1,33 @@
-import { Schema } from "mongoose";
+import { UploadResponse } from "imagekit/dist/libs/interfaces";
+import { Model, model, models, Schema } from "mongoose";
 
-const adSchema = new Schema({
+type Ad = {
+    title: string;
+    price: number;
+    category: string;
+    description: string;
+    contact: string;
+    files: UploadResponse[];
+    location: {
+        lat: number;
+        lng: number;
+    },
+    userEmail: string;
+}
+
+const adSchema = new Schema<Ad>({
     title: String,
     price: Number,
     category: String,
     description: String,
     contact: String,
+    files: [Object],
+    location: Object,
+    userEmail: {type: String, require: true}
 
 
 }, {
     timestamps: true,
-})
+});
+
+export const AdModel = (models?.Ad as Model<Ad>) || model<Ad>('Ad', adSchema);
