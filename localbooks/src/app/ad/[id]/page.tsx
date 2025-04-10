47b032a5e176f92b741b89ff77bfda8cce2e0cef
Burness <bsiii102798@gmail.com>
2025-04-10 00:26:10 -0700

    idpage

--- localbooks/src/app/ad/[id]/page.tsx
+++ localbooks/src/app/ad/[id]/page.tsx
@@ -1,12 +1,41 @@
 'use server';
+
+import { connect } from "@/libs/heplers";
+import { AdModel } from "@/models/Ad";
+
 type Props = {
     params: {
-        id:string;
+        id: string;
     };
-    searchParams: {[key: string] : string} ;
+    searchParams: { [key: string]: string };
 }
-export default async function SingleBookPage(args: Props) {
-    return(
-        <div>{JSON.stringify(args)}</div>
+export default async function AdBookPage(args: Props) {
+    await connect();
+    const adDoc = await AdModel.findById(args.params.id);
+
+    if (!adDoc) {
+        return 'Not found!'
+    }
+
+    return (
+        <div className="flex">
+            <div className="grow bg-black">photos</div>
+            <div className="w-2/5 p-8">
+                <h1 className="text-lg font-bold">
+
+                    {adDoc.title}
+                </h1>
+                <div>
+
+                    <b>Category:</b>
+                    <span>{adDoc.category}</span>
+                </div>
+                <label>description</label>
+                <p className="text-sm">{adDoc.description}</p>
+                <label>contact</label>
+                <p>{adDoc.contact}</p>
+
+            </div>
+        </div>
     )
 }
