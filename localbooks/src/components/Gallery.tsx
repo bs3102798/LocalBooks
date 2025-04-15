c7c6faa68a22c4b8df34276facce056b23bf6285
Burness <bsiii102798@gmail.com>
2025-04-14 22:59:43 -0700

    cardBackGroundImage

--- localbooks/src/components/Gallery.tsx
+++ localbooks/src/components/Gallery.tsx
@@ -6,6 +6,7 @@ import UploadView from "./UploadView";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import MyImage from "./MyImage";
 
 export default function Gallary({ files }: { files: UploadResponse[] }) {
     const [activeFile, setActiveFile] = useState<UploadResponse | null>(files?.[0] || null)
@@ -27,19 +28,30 @@ export default function Gallary({ files }: { files: UploadResponse[] }) {
             <div className="grow flex items-center relative">
                 {activeFile && (
                     <>
+                        <div className="absolute inset-0 overflow-hidden">
+
+                            <MyImage
+                                src={activeFile.filePath}
+                                alt={'bg'}
+                                width={3048}
+                                height={3048}
+                                className="object-cover opacity-20"
+                            />
+                        </div>
                         <div className="absolute inset-4 flex items-center justify-center">
+
                             <UploadView file={activeFile} />
                         </div>
                         <div className="absolute inset-4 flex items-center">
                             <div className="flex justify-between w-full">
-                                <button 
-                                onClick={prev}
-                                className="size-12 justify-center rounded-full flex items-center transition bg-gray-500/40 hover:bg-gray-500/80">
+                                <button
+                                    onClick={prev}
+                                    className="size-12 justify-center rounded-full flex items-center transition bg-gray-500/40 hover:bg-gray-500/80">
                                     <FontAwesomeIcon icon={faChevronLeft} />
                                 </button>
-                                <button 
-                                onClick={next}
-                                className="size-12 justify-center rounded-full flex items-center transition bg-gray-500/40 hover:bg-gray-500/80">
+                                <button
+                                    onClick={next}
+                                    className="size-12 justify-center rounded-full flex items-center transition bg-gray-500/40 hover:bg-gray-500/80">
                                     <FontAwesomeIcon icon={faChevronRight} />
                                 </button>
                             </div>
@@ -48,9 +60,9 @@ export default function Gallary({ files }: { files: UploadResponse[] }) {
                     </>
                 )}
             </div>
-            <div className="p-4 flex gap-4">
+            <div className="p-4 flex gap-4 justify-center">
                 {files.map((file, index) => (
-                    <div className="size-14" key={index}>
+                    <div className="size-14 cursor-pointer" key={index}>
                         <UploadThumbnail onClick={() => setActiveFile(file)}
                             file={file}
                         />
