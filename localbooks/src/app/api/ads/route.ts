54e8a7b430cebf1a7c4c55f9f27053db87c50ba4
Burness <bsiii102798@gmail.com>
2025-04-19 22:34:37 -0700

    clear consols

--- localbooks/src/app/api/ads/route.ts
+++ localbooks/src/app/api/ads/route.ts
@@ -4,13 +4,13 @@ import { FilterQuery } from "mongoose";
 
 export async function GET(req: Request,) {
     await connect();
-    //console.log(req)
+    
     const {searchParams} = new URL(req.url)
-    //console.log(url.searchParams)
+    
     const filter: FilterQuery<Ad> = {};
 
     const phrase = searchParams.get('phrase') || null
-    //console.log({phrase})
+    
     if (phrase) {
         filter.title = { $regex: '.*' + searchParams.get('phrase') + '.*', $options: 'i' }
     }
