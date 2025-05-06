335a6079cd3a79104719a41cd3a4e550f3185e31
Burness <bsiii102798@gmail.com>
2025-05-05 19:03:35 -0700

    radiusSetUp

--- localbooks/src/components/DistancePicker.tsx
+++ localbooks/src/components/DistancePicker.tsx
@@ -4,14 +4,17 @@ import { Location } from "./LocationPicker";
 
 
 const locationDefault = {
-    lat: 34.0522 , 
-    lng: -118.2437 ,
+    lat: 34.0522,
+    lng: -118.2437,
 }
 
 
 export default function DistancePicker() {
     const [radius, setRadius] = useState(10 * 1000);
-    const [center, setCenter] = useState<Location>(locationDefault)
+    const [
+        center,
+         setCenter] = useState<Location>(locationDefault)
+    const [zoom, setZoom] = useState<number>(9)
     const mapsDiv = useRef<HTMLDivElement | null>(null)
 
     useEffect(() => {
@@ -29,7 +32,7 @@ export default function DistancePicker() {
         const map = new Map(mapsDiv.current as HTMLDivElement, {
             mapId: 'map',
             center: locationDefault,
-            zoom: 8,
+            zoom: 9,
             zoomControl: true,
             mapTypeControl: false,
             streetViewControl: false,
@@ -60,13 +63,25 @@ export default function DistancePicker() {
 
         //     Core.event.addListener(circle, e, () => console.log(e))
         // }
-        
+
         Core.event.addListener(circle, 'bounds_changed', () => {
-           // console.log('radious:',circle.getRadius())
-           setRadius(circle.getRadius())
+            // console.log('radious:',circle.getRadius())
+            const radius = circle.getRadius()
+            setRadius(radius)
+            if (radius > 150000000) map.setZoom(1);
+            else if (radius > 800000) map.setZoom(2);
+            else if (radius > 400000) map.setZoom(3);
+            else if (radius > 180000) map.setZoom(4);
+            else if (radius > 100000) map.setZoom(5);
+            else if (radius > 50000) map.setZoom(6);
+            else if (radius > 25000) map.setZoom(7);
+            else if (radius > 12000) map.setZoom(8);
+            else if (radius > 5000) map.setZoom(9);
+            else if (radius <= 10000) map.setZoom(10);
+            setZoom(map.getZoom() as number)
         })
         Core.event.addListener(circle, 'center_changed', () => {
-            const circleCenter:Location|undefined = circle.getCenter()?.toJSON() 
+            const circleCenter: Location | undefined = circle.getCenter()?.toJSON()
             if (circleCenter) {
                 setCenter(circleCenter);
                 map.setCenter(circleCenter)
@@ -74,14 +89,15 @@ export default function DistancePicker() {
             }
 
 
-          
+
         })
 
     }
     return (
         <>
-        {/* center: {JSON.stringify(center)} <br />
-        radious:{JSON.stringify(radius)} */}
+            radious:{radius / 1000} <br />
+            Zoom:{zoom}
+            <label>Where</label>
             <div ref={mapsDiv} className="w-full h-48 bg-gray-200"></div>
 
         </>
