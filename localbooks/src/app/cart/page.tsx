3dd21a8a90262fbfaba2e44dfb73057a68a3c890
Burness <bsiii102798@gmail.com>
2025-06-27 23:28:54 -0700

    cartItemsOnPage

--- localbooks/src/app/cart/page.tsx
+++ localbooks/src/app/cart/page.tsx
@@ -1,7 +1,31 @@
+/* eslint-disable react/jsx-key */
+'use client'
+import { CartContext } from "@/components/CartProvider"
+import { useContext } from "react"
+
 export default function Cart() {
+    const {cartProducts} =  useContext(CartContext)
     return (
         <>
-            <h1>cart</h1>
+            <section className="mt-8">
+
+                <h1>Checkout</h1>
+                <div className="grid gap-4 grid-cols-2">
+                    <div>
+                        Products
+                        {cartProducts?.length === 0 && (
+                            <div>No Products in your cart</div>
+                        )}
+                        {cartProducts?.length > 0 && cartProducts.map((product: any) => (
+                            <div>
+                                {product.name}
+
+                            </div>
+                        ))}
+                    </div>
+                    <div>right</div>
+                </div>
+            </section>
         </>
     )
 }
