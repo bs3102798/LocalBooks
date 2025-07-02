13e61fe64342565f6059583ef9235b44a42e69b2
Burness <bsiii102798@gmail.com>
2025-07-02 04:46:17 -0700

    deleteItemsInCart

--- localbooks/src/app/cart/page.tsx
+++ localbooks/src/app/cart/page.tsx
@@ -1,26 +1,28 @@
 /* eslint-disable react/jsx-key */
 'use client'
 import { CartContext } from "@/components/CartProvider"
-import DeleteAdButton from "@/components/DeletButton"
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+//import DeleteAdButton from "@/components/DeletButton"
 import Image from "next/image"
 import { useContext } from "react"
 
 export default function Cart() {
-    const { cartProducts } = useContext(CartContext)
+    const { cartProducts, removeCartProduct } = useContext(CartContext)
 
     //console.log(cartProducts)
     return (
         <>
             <section className="mt-8">
 
-                <h1>Checkout</h1>
+                <h1 className="items-center">Checkout</h1>
                 <div className="grid gap-4 grid-cols-2">
                     <div>
                         Products
                         {cartProducts?.length === 0 && (
                             <div>No Products in your cart</div>
                         )}
-                        {cartProducts?.length > 0 && cartProducts.map((product: any) => (
+                        {cartProducts?.length > 0 && cartProducts.map((product: any, index) => (
                             <div className="flex gap-4 mb-2 border-b py-2 items-center">
                                 <div className="w-24">
                                     <Image width={"240"} height={"240"}
@@ -47,9 +49,12 @@ export default function Cart() {
                                     {product.price}
                                 </div>
 
-                                <button className="p-2">
-                                    <DeleteAdButton id={product._id} />
-
+                                <button className="p-2" type="button" onClick={() => removeCartProduct(index)}>
+                                                    <FontAwesomeIcon icon={faTrash} />
+                                                    
+                                    {/* <DeleteAdButton id={product._id} /> */}
+                                    {/* look at route.ts and find out how to add product._id to the delete
+                                    request it should be in the line const adDoc */}
                                 </button>
 
                             </div>
