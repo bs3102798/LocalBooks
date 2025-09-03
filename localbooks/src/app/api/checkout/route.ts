e7c3fa599e826edaa285cceaa0b6a471b7c9190d
Burness <bsiii102798@gmail.com>
2025-09-02 23:04:21 -0700

    order.ts

--- localbooks/src/app/api/checkout/route.ts
+++ localbooks/src/app/api/checkout/route.ts
@@ -9,21 +9,23 @@ export async function POST(req: Request) {
 
     const {cartProducts, address} = await req.json() 
 
-    const stripeSession = await stripe.checkout.session.create({
-        line_items: [],
-        mode: 'payment',
-        customer_email: '',
-        success_url: '',
-        cancel_url: '',
-        metadata:{orderId:null},
-        shipping_options: [
-            {
-                shipping_rate_data: {
-                    display_name: 'Delivery fee',
-                    type: 'fixed_amount',
-                    fixed_amount: {amount: 500, currency: 'USD'} ,
-                }
-            }
-        ]
-    })
+    
+
+    // const stripeSession = await stripe.checkout.session.create({
+    //     line_items: [],
+    //     mode: 'payment',
+    //     customer_email: '',
+    //     success_url: '',
+    //     cancel_url: '',
+    //     metadata:{orderId:null},
+    //     shipping_options: [
+    //         {
+    //             shipping_rate_data: {
+    //                 display_name: 'Delivery fee',
+    //                 type: 'fixed_amount',
+    //                 fixed_amount: {amount: 500, currency: 'USD'} ,
+    //             }
+    //         }
+    //     ]
+    // })
 }
